98ef7b74aeb62829366b145cf0ace987e0fc58c1
SamikshaAwasthi <samikshaawasthi789@gmail.com>
2026-04-19 11:54:58 +0530

    first commit

---
+++ loop/Basic/print_number.js
@@ -0,0 +1,57 @@
+// Print numbers from 1 to 100
+// let n = 100;
+// for (let i = 1; i <=n; i++) {
+//  console.log(i);  
+// }
+// Print even numbers from 1 to 50
+// let n = 50;
+// for (let i = 1; i <=n; i++) {
+//   if(i%2 ==0){
+//     console.log(i);
+    
+//   }
+// }
+// Print odd numbers from 1 to 50
+// let n = 50;
+// for (let i = 1; i <=n; i++) {
+//   if(i%2 !==0){
+//     console.log(i);
+    
+//   }
+// }
+
+// Print multiplication table of a number
+
+// let n = 5;
+// // let t= 1;
+// for (let i = 1; i <=10; i++) {
+//     t =n*i;
+//   console.log(` 5*${i} =${t}`);
+  
+// }
+
+// Find sum of first N numbers
+// let n = 10;
+// let sum = 0
+// for (let i = 1; i <=n; i++) {
+//  sum =sum+i;
+// }
+// console.log(sum);
+
+// Find factorial of a number
+// let n=4;
+// let f = 1;
+// for (let i = 1; i<=n; i++) {
+//  f = f*i
+
+// }
+// console.log(f);
+
+// Count digits in a number
+let n = 324;
+let count = 0;
+for (let i = 0; i<n; i++) {
+    
+ console.log();
+ 
+}
